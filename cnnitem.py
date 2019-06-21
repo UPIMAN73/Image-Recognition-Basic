e8f3d53ea383208de19796e2275c0648923c723e
UPIMAN73 <joshcalzadi@gmail.com>
2019-06-21 00:03:03 -0400

    Third Commit
    TODO:
    * SETUP NEURAL NETWORK
    * LOAD IMAGE ENCODING TO NN-MATRIX

---
+++ cnnitem.py
@@ -0,0 +1,54 @@
+class CNNItem:
+    def __init__(self, name, imgMatrix, filter):
+        self.name = name
+        self.imgMatrix = imgMatrix
+        self.cmatrix = convolutionLayer(imgMatrix, filter)
+        self.pmatrix = None # This must be redefined for specific values
+    
+
+    # convolutional layers
+    def convolutionLayer(matrix, filter):
+        #construct convolution matrix
+        cmatrix = []
+
+        #2d layers for any nxn matrix which n > 1
+        if (len(matrix) > 1):
+            if (len(matrix[0]) > 1 and len(matrix[1]) > 1):
+
+                # actual 2x2 matrix setup for scanning matrices
+                for i in range(0, len(matrix) - 1):
+                    for j in range(1, len(matrix[i])):
+
+                        # layers calculations based on filters
+                        msamples = [matrix[i][j - 1], matrix[i][j], matrix[i + 1][j - 1], matrix[i + 1][j]]
+                        msolution = 0
+                        for k in range(0, len(msamples)):
+                            msolution += msamples[k] * filter[k]
+                        m.append(msolution)
+        
+        # return the convoluted matrix
+        return cmatrix
+
+
+    # Pool layer is the layer that sets up the convoluted matrix to finalized processes
+    def poolLayer(cmatrix, v1, v2):
+        # two matrix values because comparing two values
+        enc1 = []
+        enc2 = []
+
+        # First comparison values
+        for i in range(0, len(cmatrix)):
+            if cmatrix[i] == v1:
+                enc1.append(1)
+            else:
+                enc1.append(-1)
+        
+        # Second comparison values
+        for i in range(0, len(cmatrix)):
+            if cmatrix[i] == v2:
+                enc2.append(1)
+            else:
+                enc2.append(-1)
+        
+        # Pool matrix
+        return [enc1, enc2]
--- cvnn_image.py
+++ cvnn_image.py
@@ -1,3 +1,4 @@
+# possibly import cnn
 
 fname = "backslash_5x5.png"
 imfile = open(fname, "rb")
@@ -8,6 +9,17 @@ def zeroArray(arry):
         res.append(0)
     return res
 
+# Encoding values for color black
+def blackScalePerc(r, g, b):
+    res = None
+    rgb_c = [r/255, g/255, b/255]
+    res = (rgb_c[0] + rgb_c[1] + rgb_c[2])/3
+    return res
+
+# Fully encoded values for black scaling
+def blackScale(score):
+    return (-2 * score) + 1
+
 def loadImage(img):
     imgMatrix = []
     imgString = img.read()
@@ -16,47 +28,6 @@ def loadImage(img):
     print("\n\n\n")
     print(imgMatrix)
     return imgMatrix
-
-# convolutional layers
-def convolutionLayer(matrix, filter):
-    #construct convolution matrix
-    cmatrix = []
-
-    #2d layers for any nxn matrix which n > 1
-    if (len(matrix) > 1):
-        if (len(matrix[0]) > 1 and len(matrix[1]) > 1):
-
-            # actual 2x2 matrix setup for scanning matrices
-            for i in range(0, len(matrix) - 1):
-                for j in range(1, len(matrix[i])):
-
-                    # layers calculations based on filters
-                    msamples = [matrix[i][j - 1], matrix[i][j], matrix[i + 1][j - 1], matrix[i + 1][j]]
-                    msolution = 0
-                    for k in range(0, len(msamples)):
-                        msolution += msamples[k] * filter[k]
-                    m.append(msolution)
-    
-    return cmatrix
-
-def poolLayer(cmatrix, v1, v2):
-    print(cmatrix)
-    enc1 = []
-    enc2 = []
-
-    for i in range(0, len(cmatrix)):
-        if cmatrix[i] == v1:
-            enc1.append(1)
-        else:
-            enc1.append(-1)
-    
-    for i in range(0, len(cmatrix)):
-        if cmatrix[i] == v2:
-            enc2.append(1)
-        else:
-            enc2.append(-1)
-    
-    return [enc1, enc2]
 
 
 # Final Layer for Neural Networks
@@ -83,17 +54,6 @@ def finalLayer(pmatrix, pools):
 
     # return the index of the network items
     return ansArray.index(max(ansArray))
-
-# Encoding values for color black
-def blackScalePerc(r, g, b):
-    res = None
-    rgb_c = [r/255, g/255, b/255]
-    res = (rgb_c[0] + rgb_c[1] + rgb_c[2])/3
-    return res
-
-# Fully encoded values for black scaling
-def blackScale(score):
-    return (-2 * score) + 1
 
 
 
